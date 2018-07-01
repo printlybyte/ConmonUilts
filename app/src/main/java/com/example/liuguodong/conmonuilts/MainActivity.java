92618584bf51c7a903e983148b2880f38a89e538
liuguodong <18701403668@163.com>
2018-07-01 18:46:20 +0800

    qqqqwdwed

--- app/src/main/java/com/example/liuguodong/conmonuilts/MainActivity.java
+++ app/src/main/java/com/example/liuguodong/conmonuilts/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.liuguodong.conmonuilts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.conmonutils.ec.CommonDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new CommonDialog.Builder(this)
+                .setTitle("标题")
+                .setMessage("这里是提示内容")
+                .setPositiveButton("确定", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }).setNegativeButton("取消", null).show();
     }
 }
